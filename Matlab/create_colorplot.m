4496902185576641c2d8eccdb3fab52fa271cce7
Max Hasslberger <max@ceregate.com>
2023-08-23 15:42:53 +0200

    15MHz long. Plot + BP filtering stage

--- Matlab/create_colorplot.m
+++ Matlab/create_colorplot.m
@@ -2,12 +2,13 @@
 path = fullfile('Data/15MHz_unfoc_char/');
 channel = 1; % oscilloscope channel
 dx = 1.0 * 1e-3; % m - dx on resulting color plot
-offset = [7.0 * 1e-3, 0.0 * 1e-3]; % m - for color plot
-center_frq = 2.0; % MHz -> Transducer frq
+offset = [6.5 * 1e-3, 0.0 * 1e-3]; % m - for color plot
+center_frq = 14.2 * 1e6; % Hz -> Transducer frq
 amp_flag = 0; % amplifier used between hydrophone - DAQ?
+fs = 100 * 1e6;
 
 % LUT for hydrophone and amplifiers
-lut.F0 = [0.5, 2.0, 15.0]; % MHz
+lut.F0 = [0.5, 2.0, 14.2] * 1e6; % Hz
 lut.gain = [530.9, 530.9, 530.9]; % NP-2519 amplifier -> ~54.5 dB (small signal)-> ~ 60 dB for very small signals!
 lut.hyd_TF = [188.4 * 1e-9, 158.5 * 1e-9, 105.9 * 1e-9]; % Onda HNR-0500 hydrophone (V/Pa) -> [~-254.5, ~-256.0, ~-259.5] dB
 
@@ -36,6 +37,13 @@ for hor = 1:length(folders)
         if size(data, 2) > 1
             data = data(:, channel);
         end
+
+        % BP filter signal
+        [b, a] = butter(7, center_frq*0.8 /(fs/2), "high");
+        data = filter(b, a, data);
+
+        [b, a] = butter(7, center_frq*1.2 /(fs/2), "low");
+        data = filter(b, a, data);
         
         % Determine amplitude and store
         max_avg = +mean(maxk(findpeaks(+data), avg_elements));
@@ -64,6 +72,7 @@ pressure = signal * lut.gain(lut_idx)^amp_flag / lut.hyd_TF(lut_idx); % Voltage 
 pcolor(((0:size(pressure, 2)-1) * dx - offset(1)) * 1e3, ((0:size(pressure, 1)-1) * dx - offset(2)) * 1e3, pressure / 1e6);
 xlabel('x (mm)');
 ylabel('y (mm)');
+title('V319-SU, Olympus (15 MHz), unfocused')
 a = colorbar;
 ylabel(a,'Pressure (MPa)','FontSize',16,'Rotation',90);
 
